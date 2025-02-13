7541b21db2d0a76267a89f9b16df6aa63d9fdcf6
Najee Douglas <48990269+Suzenko97@users.noreply.github.com>
2025-02-12 21:40:18 -0500

    Added framework for login feature
    Pages refuse to load properly, need to check

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(cors());
 app.use(express,json());
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 })
